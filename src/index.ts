92ecd5f73e2faa63bfb2fa8dee7e440213ba1da6
Christian Nazha <Christian@Nazha.dev>
2019-09-13 17:29:47 +0300

    added generate token method

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,17 @@ import * as jwt from "jsonwebtoken";
 import {IConfig} from "./interface";
 
 
-
 class JWTR {
     private readonly config: IConfig;
-    private readonly jwt;
+    private readonly jwt: any;
+
     constructor(config: IConfig) {
         this.config = config;
         this.jwt = jwt;
+    }
+
+    public generateToken(payload, jwtConfig?: any):Promise<string>  {
+        return this.jwt.sign(payload, this.config.secret, {expiresIn: '1y', ...jwtConfig});
     }
 
 }
