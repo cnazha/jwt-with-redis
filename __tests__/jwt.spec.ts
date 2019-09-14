80c54708e00c30af2382bdd296db57d505adc5fb
Christian Nazha <Christian@Nazha.dev>
2019-09-14 19:48:11 +0300

    added tests for JWT for decode/verify methods

--- __tests__/jwt.spec.ts
+++ __tests__/jwt.spec.ts
@@ -1,4 +1,5 @@
 import JWR from "../src";
+
 const jwr = new JWR({secret: "9N7-XL7epZh6#AAgMc%_2N9*-w?%Gm48z"});
 
 describe("JWT Native suite", () => {
@@ -20,7 +21,7 @@ describe("JWT Native suite", () => {
             expect(payload.data).toEqual(data);
         });
         it('should transform array payload to object', function () {
-            const data = [1,2,3,4];
+            const data = [1, 2, 3, 4];
             const payload = jwr.handlePayload(data);
             expect(payload).toBeDefined();
             expect(typeof payload).toBe("object");
@@ -43,6 +44,47 @@ describe("JWT Native suite", () => {
             const token = jwr.sign(payload);
             expect(token).toBeDefined();
             expect(typeof token).toBe("string");
+        });
+    });
+
+    describe("JWT Decode", () => {
+        it('it should return payload of type object for a token', async () => {
+            const payload = {id: 12321, name: "Christian", active: true};
+            const token = await jwr.sign(payload);
+            const data = await jwr.decode(token);
+            expect(data).toBeDefined();
+            expect(data.id).toEqual(payload.id);
+            expect(data.name).toEqual(payload.name);
+            expect(data.active).toEqual(payload.active);
+        });
+        it('it should return payload of type string for a token', async () => {
+            const payload = "Christian";
+            const token = await jwr.sign(payload);
+            const data = await jwr.decode(token);
+            expect(data).toBeDefined();
+            expect(data).toEqual(payload)
+        });
+    });
+
+    describe("JWT Verify", () => {
+        it('it should return payload for a valid token', async () => {
+            const payload = {id: 12321, name: "Christian", active: true};
+            const token = await jwr.sign(payload);
+            const data = await jwr.verify(token);
+            expect(data).toBeDefined();
+            expect(data.id).toEqual(payload.id);
+            expect(data.name).toEqual(payload.name);
+            expect(data.active).toEqual(payload.active);
+        });
+        it('it should throw a crypto error for an invalid/malformed token', async () => {
+            try {
+                const invalidToken = 'aslkdnfln32k4no110rjfdisan';
+                const data = await jwr.verify(invalidToken);
+            } catch (e) {
+                expect(() => {
+                    e.toThrow(TypeError)
+                });
+            }
         });
     });
 
