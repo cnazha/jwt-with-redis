8e098918471efe7b199c9e989e20a8a072177322
Christian Nazha <Christian@Nazha.dev>
2019-09-14 18:24:01 +0300

    added first jest test for jwt sign method

--- __tests__/jwt.spec.ts
+++ __tests__/jwt.spec.ts
@@ -1,3 +1,11 @@
-test('should return false given external link', () => {
-    expect(false).toBe(false)
+import JWR from "../src";
+const jwr = new JWR({secret: "9N7-XL7epZh6#AAgMc%_2N9*-w?%Gm48z"});
+
+describe("JWT Native suite", () => {
+    test('it should return a token when signing payload', () => {
+        const payload = {id: 12321, name: "Christian", role: "user"};
+        const token = jwr.sign(payload);
+        expect(token).toBeDefined();
+        expect(typeof token).toBe("string");
+    });
 });
