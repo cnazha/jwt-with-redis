4a3f91978a8a296153e43694cb482d59ff97baa8
Christian Nazha <Christian@Nazha.dev>
2019-09-14 19:30:10 +0300

    added method to revert payload to original data

--- src/JWT.ts
+++ src/JWT.ts
@@ -1,53 +1,61 @@
 import * as jwt from "jsonwebtoken";
-import {IConfig} from "./interface";
+import { IConfig } from "./interface";
 
 export default class JWT {
-    private readonly jwt: any;
-    protected readonly config: IConfig;
-    protected readonly defaultJWTConfig: any;
-    protected readonly SECRET: string;
+  private readonly jwt: any;
+  protected readonly config: IConfig;
+  protected readonly defaultJWTConfig: any;
+  protected readonly SECRET: string;
 
-    constructor(config: IConfig) {
-        this.config = config;
-        this.SECRET = config.secret;
-        this.jwt = jwt;
-        this.defaultJWTConfig = {expiresIn: 60 * 60 * 24};
-    }
+  constructor(config: IConfig) {
+    this.config = config;
+    this.SECRET = config.secret;
+    this.jwt = jwt;
+    this.defaultJWTConfig = { expiresIn: 60 * 60 * 24 };
+  }
 
-    // Create object if payload is a string to support expiry
-    public handlePayload(payload) {
-        // Is an object and not array
-        const isObject = typeof payload === "object" &&
-            payload !== null &&
-            !Array.isArray(payload);
+  // Create object if payload is a string to support expiry
+  public handlePayload(payload) {
+    // Is an object and not array
+    const isObject =
+      typeof payload === "object" &&
+      payload !== null &&
+      !Array.isArray(payload);
 
-        // Return object
-        if (isObject) return payload;
+    // Return object
+    if (isObject) return payload;
 
-        // Return as transformed object
-        return {
-            data: payload,
-            jwr_objectified: true
-        };
-    }
+    // Return as transformed object
+    return {
+      data: payload,
+      jwr_objectified: true
+    };
+  }
 
-    // JWT sign method
-    public sign(rawPayload, jwtConfig: any = {}): Promise<string> {
-        const options = {
-            ...this.defaultJWTConfig,
-            ...jwtConfig
-        };
-        const payload = this.handlePayload(rawPayload);
-        return this.jwt.sign(payload, this.SECRET, options);
-    }
+  public revertPayload(payload) {
+    if (payload.jwr_objectified) return payload.data;
+    return payload;
+  }
 
-    // JWT decode method
-    public decode(token: string): Promise<string> {
-        return this.jwt.decode(token, this.SECRET);
-    }
+  // JWT sign method
+  public sign(rawPayload, jwtConfig: any = {}): Promise<string> {
+    const options = {
+      ...this.defaultJWTConfig,
+      ...jwtConfig
+    };
+    const payload = this.handlePayload(rawPayload);
+    return this.jwt.sign(payload, this.SECRET, options);
+  }
 
-    // JWT verify method
-    public verify(token: string): Promise<string> {
-        return this.jwt.verify(token, this.SECRET);
-    }
+  // JWT decode method
+  public decode(token: string): Promise<string> {
+    const payload = this.jwt.decode(token, this.SECRET);
+    return this.revertPayload(payload);
+  }
+
+  // JWT verify method
+  public verify(token: string): Promise<string> {
+    const payload = this.jwt.verify(token, this.SECRET);
+    return this.revertPayload(payload);
+  }
 }
