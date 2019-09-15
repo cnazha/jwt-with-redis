c3ff7cab6ed283ed641d7bbb8f9a56d96b6fb106
Christian Nazha <Christian@Nazha.dev>
2019-09-15 08:44:34 +0300

    extended interface for custom jwt sign option to use numbers only on expiry

--- src/interface.ts
+++ src/interface.ts
@@ -1,5 +1,13 @@
+import {SignOptions} from "jsonwebtoken";
+
+
 export interface IConfig {
   prefix?: string;
   secret?: string;
   env_key?: string
+}
+
+export interface ISignOptions extends SignOptions{
+  expiresIn?: number // In seconds, to be compatible with redis
+  notBefore?: number // In seconds, to be compatible with redis
 }
